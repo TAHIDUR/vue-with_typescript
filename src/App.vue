e3415d87b3805195bd69e16f473f277c873724f6
tahidur <tahidurrahman.tuhin@gmail.com>
2022-02-26 15:01:24 +0600

    reactive variable with typescript

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref, reactive, onMounted } from "vue";
+import fetchCount from "./fetchCount";
+
+
+interface AppInfo {
+  name: string,
+  slogan: string,
+}
+
+const count = ref<number | null>(null)
+
+const info: AppInfo = reactive({
+  name: 'Tuhin',
+  slogan: 'Vue with Typescript',
+})
+
+onMounted(() => {
+  fetchCount((initialCount) => {
+    count.value = initialCount
+  })
+})
+
+</script>
+
+<template>
+  <h1>{{ info.name }}</h1>
+  <h2>{{ info.slogan }}</h2>
+  <p>{{ count }}</p>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/fetchCount.ts
@@ -0,0 +1,8 @@
+interface FetchCountCallback {
+    (count: number): void
+}
+
+
+export default function fetchCount(cb: FetchCountCallback) {
+    cb(0)
+}
